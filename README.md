f8c69d8af2caeec23dc86b3070f9d9061aacea61
nazmul1010 <nazmulhasan169369@gmail.com>
2025-12-28 14:27:55 +0600

    make api and add limiter

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # NH Notification Package
 
-**Version:** 1.8.2  
+**Version:** 1.8.5  
 **Author:** [Nazmul Hasan](mailto:nazmulhasan169369@gmail.com)  
 **License:** MIT  
 **Framework:** Laravel 10 / 11  
--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\NotificationController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 
-Route::middleware(['auth:sanctum', 'throttle:60,1'])->group(function () {
+Route::middleware(['auth:sanctum', 'throttle:60,1'])->prefix('api')->group(function () {
     Route::get('notifications', [NotificationController::class, 'notifications']);
     Route::get('notification/read/{item}', [NotificationController::class, 'read']);
     Route::get('notification/delete/{item}', [NotificationController::class, 'delete']);
